89c8e8a7337f5f5ae9fe6ec7dba6f70741d3ed4a
goblin.brn <bschenck0@gmail.com>
2022-05-15 16:11:56 -0700

    fixed push bug. was problem with brakets in db.json

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 // import dependencies
 const express = require('express');
-const {v4: uuid} = require('uuid');
+const {v4: uuidv4} = require('uuid');
 const path = require('path');
 const fs = require('fs');
-// const { json } = require('express/lib/response');
 
 // config
 const PORT = process.env.PORT || 3001;
@@ -16,37 +15,51 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// send GET request
-app.get('/notes', (req,res) => {
+// send GET requests
+app.get('/', (req,res) => {
     // Send file 'notes.html'
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('*', (req,res) => {
+app.get('/notes', (req,res) => {
     // Send file 'notes.html'
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('/api/notes', (req,res) => {
     // Send file 'notes.html'
-    res.sendFile(path.join(__dirname, '/db/db.json'));
+    res.sendFile(path.join(__dirname, './db/db.json'));
 });
 
 // send POST request
-app.post('/api/notes', (res,req) => {
-    // Create (persist) data
-    // Access new note data from request
-    fs.readFileSync(path.join(__dirname, '/db/db.json'), (err, data) => {
-        if (err) {
-            throw err;
-        } else {
-            // Push it to my existing list of notes
-            var note = JSON.parse(data);
-            note.push({...req.body, id: uuidv4()});
-            fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(note));
-        }
-    });
-    // Write my updates note list to the `db.json` file
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    let notesDb = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const note = { title, text, id: uuidv4(), };
+
+        notesDb.push(note);
+        const allNotes = JSON.stringify(notesDb, null, 4)
+
+        fs.writeFileSync('./db/db.json', allNotes, 'utf8', (err) => 
+            err
+                ? console.err(err)
+                : console.log("Note created")
+        );
+
+        const response = {
+            status: 'success',
+            body: note,
+        };
+
+        res.status(201).json(response);
+
+    } else {
+        res.status(500).json("Error: Please check connection and retry.")
+    }
 });
 
 app.listen(PORT, () => 
