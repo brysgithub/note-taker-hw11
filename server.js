62b3bd1b415961f99af450622145267fec11b3e4
goblin.brn <bschenck0@gmail.com>
2022-05-15 13:00:23 -0700

    figured out why post route was broken. res.readFile changed to fs.readFile

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 // import dependencies
 const express = require('express');
+const {v4: uuid} = require('uuid');
 const path = require('path');
 const fs = require('fs');
+// const { json } = require('express/lib/response');
 
-const app = express();
+// config
 const PORT = process.env.PORT || 3001;
+
+// wrapper
+const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // send GET request
 app.get('/notes', (req,res) => {
@@ -30,12 +36,21 @@ app.get('/api/notes', (req,res) => {
 app.post('/api/notes', (res,req) => {
     // Create (persist) data
     // Access new note data from request
-    // Push it to my existing lisst of notes
+    fs.readFileSync(path.join(__dirname, '/db/db.json'), (err, data) => {
+        if (err) {
+            throw err;
+        } else {
+            // Push it to my existing list of notes
+            var note = JSON.parse(data);
+            note.push({...req.body, id: uuidv4()});
+            fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(note));
+        }
+    });
     // Write my updates note list to the `db.json` file
 });
 
 app.listen(PORT, () => 
-    console.log(`Example app listening at http://localhost:${PORT}`)
+    console.log(`Note Taker v1 listening at http://localhost:${PORT}`)
 );
 
 // Bonus credit for delete note functionality
